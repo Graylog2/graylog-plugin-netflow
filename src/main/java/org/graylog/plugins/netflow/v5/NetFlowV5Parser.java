4be1a0c970a42f193da1a8840380efa6e350b430
Jochen Schalanda <jochen@graylog.com>
2017-07-19 14:47:37 +0200

    Add data descriptions to NetFlowV5Parser and NetFlowV9Parser

--- src/main/java/org/graylog/plugins/netflow/v5/NetFlowV5Parser.java
+++ src/main/java/org/graylog/plugins/netflow/v5/NetFlowV5Parser.java
@@ -46,6 +46,21 @@ public class NetFlowV5Parser {
         return NetFlowV5Packet.create(header, records.build(), offset);
     }
 
+    /**
+     * <pre>
+     * | BYTES |     CONTENTS      |                                       DESCRIPTION                                        |
+     * |-------|-------------------|------------------------------------------------------------------------------------------|
+     * | 0-1   | version           | NetFlow export format version number                                                     |
+     * | 2-3   | count             | Number of flows exported in this packet (1-30)                                           |
+     * | 4-7   | sys_uptime        | Current time in milliseconds since the export device booted                              |
+     * | 8-11  | unix_secs         | Current count of seconds since 0000 UTC 1970                                             |
+     * | 12-15 | unix_nsecs        | Residual nanoseconds since 0000 UTC 1970                                                 |
+     * | 16-19 | flow_sequence     | Sequence counter of total flows seen                                                     |
+     * | 20    | engine_type       | Type of flow-switching engine                                                            |
+     * | 21    | engine_id         | Slot number of the flow-switching engine                                                 |
+     * | 22-23 | sampling_interval | First two bits hold the sampling mode; remaining 14 bits hold value of sampling interval |
+     * </pre>
+     */
     public static NetFlowV5Header parseHeader(ByteBuf bb) {
         final int version = bb.readUnsignedShort();
         if (version != 5) {
@@ -76,6 +91,32 @@ public class NetFlowV5Parser {
                 samplingInterval);
     }
 
+    /**
+     * <pre>
+     * | BYTES | CONTENTS  |                            DESCRIPTION                             |
+     * |-------|-----------|--------------------------------------------------------------------|
+     * | 0-3   | srcaddr   | Source IP address                                                  |
+     * | 4-7   | dstaddr   | Destination IP address                                             |
+     * | 8-11  | nexthop   | IP address of next hop router                                      |
+     * | 12-13 | input     | SNMP index of input interface                                      |
+     * | 14-15 | output    | SNMP index of output interface                                     |
+     * | 16-19 | dPkts     | Packets in the flow                                                |
+     * | 20-23 | dOctets   | Total number of Layer 3 bytes in the packets of the flow           |
+     * | 24-27 | first     | SysUptime at start of flow                                         |
+     * | 28-31 | last      | SysUptime at the time the last packet of the flow was received     |
+     * | 32-33 | srcport   | TCP/UDP source port number or equivalent                           |
+     * | 34-35 | dstport   | TCP/UDP destination port number or equivalent                      |
+     * | 36    | pad1      | Unused (zero) bytes                                                |
+     * | 37    | tcp_flags | Cumulative OR of TCP flags                                         |
+     * | 38    | prot      | IP protocol type (for example, TCP = 6; UDP = 17)                  |
+     * | 39    | tos       | IP type of service (ToS)                                           |
+     * | 40-41 | src_as    | Autonomous system number of the source, either origin or peer      |
+     * | 42-43 | dst_as    | Autonomous system number of the destination, either origin or peer |
+     * | 44    | src_mask  | Source address prefix mask bits                                    |
+     * | 45    | dst_mask  | Destination address prefix mask bits                               |
+     * | 46-47 | pad2      | Unused (zero) bytes                                                |
+     * </pre>
+     */
     public static NetFlowV5Record parseRecord(ByteBuf bb) {
         final InetAddress srcAddr = ByteBufUtils.readInetAddress(bb);
         final InetAddress dstAddr = ByteBufUtils.readInetAddress(bb);
