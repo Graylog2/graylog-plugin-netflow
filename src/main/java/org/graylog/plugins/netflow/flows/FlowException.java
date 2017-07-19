d85f7e802ae477aedba89b1e4951e653d182cec9
Jochen Schalanda <jochen@graylog.com>
2017-07-19 12:10:20 +0200

    Remove unused constructor from FlowException

--- src/main/java/org/graylog/plugins/netflow/flows/FlowException.java
+++ src/main/java/org/graylog/plugins/netflow/flows/FlowException.java
@@ -24,8 +24,4 @@ public class FlowException extends RuntimeException {
     public FlowException(String message) {
         super(message);
     }
-
-    public FlowException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
